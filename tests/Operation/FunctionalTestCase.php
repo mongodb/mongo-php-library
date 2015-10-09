50ef5ed61ab090cf36cf38db56cf269186a506af
Jeremy Mikola <jmikola@gmail.com>
2015-10-09 19:30:54 -0400

    Refactor functional tests to use DropCollection

--- tests/Operation/FunctionalTestCase.php
+++ tests/Operation/FunctionalTestCase.php
@@ -2,7 +2,9 @@
 
 namespace MongoDB\Tests\Operation;
 
+use MongoDB\Collection;
 use MongoDB\Driver\ReadPreference;
+use MongoDB\Operation\DropCollection;
 use MongoDB\Tests\FunctionalTestCase as BaseFunctionalTestCase;
 
 /**
@@ -10,8 +12,21 @@ use MongoDB\Tests\FunctionalTestCase as BaseFunctionalTestCase;
  */
 abstract class FunctionalTestCase extends BaseFunctionalTestCase
 {
-    public function getPrimaryServer()
+    public function setUp()
     {
-        return $this->manager->selectServer(new ReadPreference(ReadPreference::RP_PRIMARY));
+        parent::setUp();
+
+        $operation = new DropCollection($this->getDatabaseName(), $this->getCollectionName());
+        $operation->execute($this->getPrimaryServer());
+    }
+
+    public function tearDown()
+    {
+        if ($this->hasFailed()) {
+            return;
+        }
+
+        $operation = new DropCollection($this->getDatabaseName(), $this->getCollectionName());
+        $operation->execute($this->getPrimaryServer());
     }
 }

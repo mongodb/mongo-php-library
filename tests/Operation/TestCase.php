ae97a8d9acbdbc9a35ec8e85b6566175a5d52e80
Jeremy Mikola <jmikola@gmail.com>
2015-12-16 13:43:28 -0500

    Move invalid data providers to base TestCase class

--- tests/Operation/TestCase.php
+++ tests/Operation/TestCase.php
@@ -3,50 +3,10 @@
 namespace MongoDB\Tests\Operation;
 
 use MongoDB\Tests\TestCase as BaseTestCase;
-use stdClass;
 
 /**
  * Base class for Operation unit tests.
  */
 abstract class TestCase extends BaseTestCase
 {
-    public function provideInvalidDocumentValues()
-    {
-        return $this->wrapValuesForDataProvider($this->getInvalidDocumentValues());
-    }
-
-    public function provideInvalidBooleanValues()
-    {
-        return $this->wrapValuesForDataProvider($this->getInvalidBooleanValues());
-    }
-
-    protected function getInvalidArrayValues()
-    {
-        return [123, 3.14, 'foo', true, new stdClass];
-    }
-
-    protected function getInvalidBooleanValues()
-    {
-        return [123, 3.14, 'foo', [], new stdClass];
-    }
-
-    protected function getInvalidDocumentValues()
-    {
-        return [123, 3.14, 'foo', true];
-    }
-
-    protected function getInvalidIntegerValues()
-    {
-        return [3.14, 'foo', true, [], new stdClass];
-    }
-
-    protected function getInvalidStringValues()
-    {
-        return [123, 3.14, true, [], new stdClass];
-    }
-
-    protected function wrapValuesForDataProvider(array $values)
-    {
-        return array_map(function($value) { return [$value]; }, $values);
-    }
 }

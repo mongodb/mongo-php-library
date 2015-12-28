afb74871db30731feadff4daf6fbbfb44b07503d
Jeremy Mikola <jmikola@gmail.com>
2015-12-28 14:35:46 -0500

    PHPLIB-74: Model classes for BSON array and document

---
+++ src/Model/BSONArray.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace MongoDB\Model;
+
+use MongoDB\BSON\Serializable;
+use MongoDB\BSON\Unserializable;
+use ArrayObject;
+
+/**
+ * Model class for a BSON array.
+ *
+ * The internal data will be filtered through array_values() during BSON
+ * serialization to ensure that it becomes a BSON array.
+ *
+ * @api
+ */
+class BSONArray extends ArrayObject implements Serializable, Unserializable
+{
+    /**
+     * Serialize the array to BSON.
+     *
+     * The array data will be numerically reindexed to ensure that it is stored
+     * as a BSON array.
+     *
+     * @see http://php.net/mongodb-bson-serializable.bsonserialize
+     * @return array
+     */
+    public function bsonSerialize()
+    {
+        return array_values($this->getArrayCopy());
+    }
+
+    /**
+     * Unserialize the document to BSON.
+     *
+     * @see http://php.net/mongodb-bson-unserializable.bsonunserialize
+     * @param array $data Array data
+     */
+    public function bsonUnserialize(array $data)
+    {
+        self::__construct($data);
+    }
+}

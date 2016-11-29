mongodb/mongo-php-library

commit 21b7d92aef4def00abade10911bae41f1b5ea378

PHPLIB-200: Support writeConcern option for commands that write

src/Client.php
@@ -16,6 +16,7 @@ class Client
         'document' => 'MongoDB\Model\BSONDocument',
         'root' => 'MongoDB\Model\BSONDocument',
     ];
+    private static $wireVersionForWritableCommandWriteConcern = 5;
 
     private $manager;
     private $uri;
@@ -114,8 +115,13 @@ class Client
             $options['typeMap'] = $this->typeMap;
         }
 
-        $operation = new DropDatabase($databaseName, $options);
         $server = $this->manager->selectServer(new ReadPreference(ReadPreference::RP_PRIMARY));
+
+        if ( ! isset($options['writeConcern']) && \MongoDB\server_supports_feature($server, self::$wireVersionForWritableCommandWriteConcern)) {
+            $options['writeConcern'] = $this->writeConcern;
+        }
+
+        $operation = new DropDatabase($databaseName, $options);
 
         return $operation->execute($server);
     }

src/Collection.php
@@ -39,6 +39,7 @@ class Collection
         'root' => 'MongoDB\Model\BSONDocument',
     ];
     private static $wireVersionForFindAndModifyWriteConcern = 4;
+    private static $wireVersionForWritableCommandWriteConcern = 5;
 
     private $collectionName;
     private $databaseName;
@@ -180,8 +181,13 @@ class Collection
             $options['typeMap'] = $this->typeMap;
         }
 
-        $operation = new Aggregate($this->databaseName, $this->collectionName, $pipeline, $options);
         $server = $this->manager->selectServer($options['readPreference']);
+
+        if ($hasOutStage && ! isset($options['writeConcern']) && \MongoDB\server_supports_feature($server, self::$wireVersionForWritableCommandWriteConcern)) {
+            $options['writeConcern'] = $this->writeConcern;
+        }
+
+        $operation = new Aggregate($this->databaseName, $this->collectionName, $pipeline, $options);
 
         return $operation->execute($server);
     }
@@ -234,6 +240,7 @@ class Collection
      * Create a single index for the collection.
      *
      * @see Collection::createIndexes()
+     * @see CreateIndexes::__construct() for supported command options
      * @param array|object $key     Document containing fields mapped to values,
      *                              which denote order or an index type
      * @param array        $options Index options
@@ -241,7 +248,10 @@ class Collection
      */
     public function createIndex($key, array $options = [])
     {
-        return current($this->createIndexes([['key' => $key] + $options]));
+        $indexOptions = array_diff_key($options, ['writeConcern' => 1]);
+        $commandOptions = array_intersect_key($options, ['writeConcern' => 1]);
+
+        return current($this->createIndexes([['key' => $key] + $indexOptions], $commandOptions));
     }
 
     /**
@@ -263,14 +273,21 @@ class Collection
      *
      * @see http://docs.mongodb.org/manual/reference/command/createIndexes/
      * @see http://docs.mongodb.org/manual/reference/method/db.collection.createIndex/
+     * @see CreateIndexes::__construct() for supported command options
      * @param array[] $indexes List of index specifications
+     * @param array   $options Command options
      * @return string[] The names of the created indexes
      * @throws InvalidArgumentException if an index specification is invalid
      */
-    public function createIndexes(array $indexes)
+    public function createIndexes(array $indexes, array $options = [])
     {
-        $operation = new CreateIndexes($this->databaseName, $this->collectionName, $indexes);
         $server = $this->manager->selectServer(new ReadPreference(ReadPreference::RP_PRIMARY));
+
+        if ( ! isset($options['writeConcern']) && \MongoDB\server_supports_feature($server, self::$wireVersionForWritableCommandWriteConcern)) {
+            $options['writeConcern'] = $this->writeConcern;
+        }
+
+        $operation = new CreateIndexes($this->databaseName, $this->collectionName, $indexes, $options);
 
         return $operation->execute($server);
     }
@@ -355,8 +372,13 @@ class Collection
             $options['typeMap'] = $this->typeMap;
         }
 
-        $operation = new DropCollection($this->databaseName, $this->collectionName, $options);
         $server = $this->manager->selectServer(new ReadPreference(ReadPreference::RP_PRIMARY));
+
+        if ( ! isset($options['writeConcern']) && \MongoDB\server_supports_feature($server, self::$wireVersionForWritableCommandWriteConcern)) {
+            $options['writeConcern'] = $this->writeConcern;
+        }
+
+        $operation = new DropCollection($this->databaseName, $this->collectionName, $options);
 
         return $operation->execute($server);
     }
@@ -382,8 +404,13 @@ class Collection
             $options['typeMap'] = $this->typeMap;
         }
 
-        $operation = new DropIndexes($this->databaseName, $this->collectionName, $indexName, $options);
         $server = $this->manager->selectServer(new ReadPreference(ReadPreference::RP_PRIMARY));
+
+        if ( ! isset($options['writeConcern']) && \MongoDB\server_supports_feature($server, self::$wireVersionForWritableCommandWriteConcern)) {
+            $options['writeConcern'] = $this->writeConcern;
+        }
+
+        $operation = new DropIndexes($this->databaseName, $this->collectionName, $indexName, $options);
 
         return $operation->execute($server);
     }
@@ -401,8 +428,13 @@ class Collection
             $options['typeMap'] = $this->typeMap;
         }
 
-        $operation = new DropIndexes($this->databaseName, $this->collectionName, '*', $options);
         $server = $this->manager->selectServer(new ReadPreference(ReadPreference::RP_PRIMARY));
+
+        if ( ! isset($options['writeConcern']) && \MongoDB\server_supports_feature($server, self::$wireVersionForWritableCommandWriteConcern)) {
+            $options['writeConcern'] = $this->writeConcern;
+        }
+
+        $operation = new DropIndexes($this->databaseName, $this->collectionName, '*', $options);
 
         return $operation->execute($server);
     }

src/Database.php
@@ -24,6 +24,7 @@ class Database
         'document' => 'MongoDB\Model\BSONDocument',
         'root' => 'MongoDB\Model\BSONDocument',
     ];
+    private static $wireVersionForWritableCommandWriteConcern = 5;
 
     private $databaseName;
     private $manager;
@@ -173,8 +174,13 @@ class Database
             $options['typeMap'] = $this->typeMap;
         }
 
-        $operation = new CreateCollection($this->databaseName, $collectionName, $options);
         $server = $this->manager->selectServer(new ReadPreference(ReadPreference::RP_PRIMARY));
+
+        if ( ! isset($options['writeConcern']) && \MongoDB\server_supports_feature($server, self::$wireVersionForWritableCommandWriteConcern)) {
+            $options['writeConcern'] = $this->writeConcern;
+        }
+
+        $operation = new CreateCollection($this->databaseName, $collectionName, $options);
 
         return $operation->execute($server);
     }
@@ -192,8 +198,13 @@ class Database
             $options['typeMap'] = $this->typeMap;
         }
 
-        $operation = new DropDatabase($this->databaseName, $options);
         $server = $this->manager->selectServer(new ReadPreference(ReadPreference::RP_PRIMARY));
+
+        if ( ! isset($options['writeConcern']) && \MongoDB\server_supports_feature($server, self::$wireVersionForWritableCommandWriteConcern)) {
+            $options['writeConcern'] = $this->writeConcern;
+        }
+
+        $operation = new DropDatabase($this->databaseName, $options);
 
         return $operation->execute($server);
     }
@@ -212,8 +223,13 @@ class Database
             $options['typeMap'] = $this->typeMap;
         }
 
-        $operation = new DropCollection($this->databaseName, $collectionName, $options);
         $server = $this->manager->selectServer(new ReadPreference(ReadPreference::RP_PRIMARY));
+
+        if ( ! isset($options['writeConcern']) && \MongoDB\server_supports_feature($server, self::$wireVersionForWritableCommandWriteConcern)) {
+            $options['writeConcern'] = $this->writeConcern;
+        }
+
+        $operation = new DropCollection($this->databaseName, $collectionName, $options);
 
         return $operation->execute($server);
     }

src/Exception/UnsupportedException.php
@@ -13,4 +13,14 @@ class UnsupportedException extends RuntimeException implements Exception
     {
         return new static('Collations are not supported by the server executing this operation');
     }
+
+    /**
+     * Thrown when a command's writeConcern option is not supported by a server.
+     *
+     * @return self
+     */
+    public static function writeConcernNotSupported()
+    {
+        return new static('Write concern is not supported by the server executing this command');
+    }
 }

src/Operation/Aggregate.php
@@ -6,6 +6,7 @@ use MongoDB\Driver\Command;
 use MongoDB\Driver\ReadConcern;
 use MongoDB\Driver\ReadPreference;
 use MongoDB\Driver\Server;
+use MongoDB\Driver\WriteConcern;
 use MongoDB\Exception\InvalidArgumentException;
 use MongoDB\Exception\UnexpectedValueException;
 use MongoDB\Exception\UnsupportedException;
@@ -26,6 +27,7 @@ class Aggregate implements Executable
     private static $wireVersionForCursor = 2;
     private static $wireVersionForDocumentLevelValidation = 4;
     private static $wireVersionForReadConcern = 4;
+    private static $wireVersionForWriteConcern = 5;
 
     private $databaseName;
     private $collectionName;
@@ -80,6 +82,12 @@ class Aggregate implements Executable
      *
      *    For servers >= 2.6, this option allows users to turn off cursors if
      *    necessary to aid in mongod/mongos upgrades.
+     *
+     *  * writeConcern (MongoDB\Driver\WriteConcern): Write concern. This only
+     *    applies when the $out stage is specified.
+     *
+     *    This is not supported for server versions < 3.4 and will result in an
+     *    exception at execution time if used.
      *
      * @param string $databaseName   Database name
      * @param string $collectionName Collection name
@@ -148,6 +156,10 @@ class Aggregate implements Executable
             throw InvalidArgumentException::invalidType('"useCursor" option', $options['useCursor'], 'boolean');
         }
 
+        if (isset($options['writeConcern']) && ! $options['writeConcern'] instanceof WriteConcern) {
+            throw InvalidArgumentException::invalidType('"writeConcern" option', $options['writeConcern'], 'MongoDB\Driver\WriteConcern');
+        }
+
         if (isset($options['batchSize']) && ! $options['useCursor']) {
             throw new InvalidArgumentException('"batchSize" option should not be used if "useCursor" is false');
         }
@@ -169,12 +181,16 @@ class Aggregate implements Executable
      * @param Server $server
      * @return Traversable
      * @throws UnexpectedValueException if the command response was malformed
-     * @throws UnsupportedException if collation is used and unsupported
+     * @throws UnsupportedException if collation or write concern is used and unsupported
      */
     public function execute(Server $server)
     {
         if (isset($this->options['collation']) && ! \MongoDB\server_supports_feature($server, self::$wireVersionForCollation)) {
             throw UnsupportedException::collationNotSupported();
+        }
+
+        if (isset($this->options['writeConcern']) && ! \MongoDB\server_supports_feature($server, self::$wireVersionForWriteConcern)) {
+            throw UnsupportedException::writeConcernNotSupported();
         }
 
         $isCursorSupported = \MongoDB\server_supports_feature($server, self::$wireVersionForCursor);
@@ -238,6 +254,10 @@ class Aggregate implements Executable
 
         if (isset($this->options['readConcern']) && \MongoDB\server_supports_feature($server, self::$wireVersionForReadConcern)) {
             $cmd['readConcern'] = \MongoDB\read_concern_as_document($this->options['readConcern']);
+        }
+
+        if (isset($this->options['writeConcern'])) {
+            $cmd['writeConcern'] = \MongoDB\write_concern_as_document($this->options['writeConcern']);
         }
 
         if ($this->options['useCursor']) {

src/Operation/CreateCollection.php
@@ -4,6 +4,7 @@ namespace MongoDB\Operation;
 
 use MongoDB\Driver\Command;
 use MongoDB\Driver\Server;
+use MongoDB\Driver\WriteConcern;
 use MongoDB\Exception\InvalidArgumentException;
 use MongoDB\Exception\UnsupportedException;
 
@@ -20,6 +21,7 @@ class CreateCollection implements Executable
     const NO_PADDING = 2;
 
     private static $wireVersionForCollation = 5;
+    private static $wireVersionForWriteConcern = 5;
 
     private $databaseName;
     private $collectionName;
@@ -68,6 +70,11 @@ class CreateCollection implements Executable
      *  * validationLevel (string): Validation level.
      *
      *  * validator (document): Validation rules or expressions.
+     *
+     *  * writeConcern (MongoDB\Driver\WriteConcern): Write concern.
+     *
+     *    This is not supported for server versions < 3.4 and will result in an
+     *    exception at execution time if used.
      *
      * @see http://source.wiredtiger.com/2.4.1/struct_w_t___s_e_s_s_i_o_n.html#a358ca4141d59c345f401c58501276bbb
      * @see https://docs.mongodb.org/manual/core/document-validation/
@@ -130,6 +137,10 @@ class CreateCollection implements Executable
             throw InvalidArgumentException::invalidType('"validator" option', $options['validator'], 'array or object');
         }
 
+        if (isset($options['writeConcern']) && ! $options['writeConcern'] instanceof WriteConcern) {
+            throw InvalidArgumentException::invalidType('"writeConcern" option', $options['writeConcern'], 'MongoDB\Driver\WriteConcern');
+        }
+
         $this->databaseName = (string) $databaseName;
         $this->collectionName = (string) $collectionName;
         $this->options = $options;
@@ -141,12 +152,16 @@ class CreateCollection implements Executable
      * @see Executable::execute()
      * @param Server $server
      * @return array|object Command result document
-     * @throws UnsupportedException if collation is used and unsupported
+     * @throws UnsupportedException if collation or write concern is used and unsupported
      */
     public function execute(Server $server)
     {
         if (isset($this->options['collation']) && ! \MongoDB\server_supports_feature($server, self::$wireVersionForCollation)) {
             throw UnsupportedException::collationNotSupported();
+        }
+
+        if (isset($this->options['writeConcern']) && ! \MongoDB\server_supports_feature($server, self::$wireVersionForWriteConcern)) {
+            throw UnsupportedException::writeConcernNotSupported();
         }
 
         $cursor = $server->executeCommand($this->databaseName, $this->createCommand());
@@ -177,6 +192,10 @@ class CreateCollection implements Executable
             if (isset($this->options[$option])) {
                 $cmd[$option] = (object) $this->options[$option];
             }
+        }
+
+        if (isset($this->options['writeConcern'])) {
+            $cmd['writeConcern'] = \MongoDB\write_concern_as_document($this->options['writeConcern']);
         }
 
         return new Command($cmd);

src/Operation/CreateIndexes.php
@@ -22,21 +22,31 @@ class CreateIndexes implements Executable
 {
     private static $wireVersionForCollation = 5;
     private static $wireVersionForCommand = 2;
+    private static $wireVersionForWriteConcern = 5;
 
     private $databaseName;
     private $collectionName;
     private $indexes = [];
     private $isCollationUsed = false;
+    private $options = [];
 
     /**
      * Constructs a createIndexes command.
      *
+     * Supported options:
+     *
+     *  * writeConcern (MongoDB\Driver\WriteConcern): Write concern.
+     *
+     *    This is not supported for server versions < 3.4 and will result in an
+     *    exception at execution time if used.
+     *
      * @param string  $databaseName   Database name
      * @param string  $collectionName Collection name
      * @param array[] $indexes        List of index specifications
+     * @param array   $options        Command options
      * @throws InvalidArgumentException
      */
-    public function __construct($databaseName, $collectionName, array $indexes)
+    public function __construct($databaseName, $collectionName, array $indexes, array $options = [])
     {
         if (empty($indexes)) {
             throw new InvalidArgumentException('$indexes is empty');
@@ -66,8 +76,13 @@ class CreateIndexes implements Executable
             $expectedIndex += 1;
         }
 
+        if (isset($options['writeConcern']) && ! $options['writeConcern'] instanceof WriteConcern) {
+            throw InvalidArgumentException::invalidType('"writeConcern" option', $options['writeConcern'], 'MongoDB\Driver\WriteConcern');
+        }
+
         $this->databaseName = (string) $databaseName;
         $this->collectionName = (string) $collectionName;
+        $this->options = $options;
     }
 
     /**
@@ -79,12 +94,16 @@ class CreateIndexes implements Executable
      * @see Executable::execute()
      * @param Server $server
      * @return string[] The names of the created indexes
-     * @throws UnsupportedException if collation is used and unsupported
+     * @throws UnsupportedException if collation or write concern is used and unsupported
      */
     public function execute(Server $server)
     {
         if ($this->isCollationUsed && ! \MongoDB\server_supports_feature($server, self::$wireVersionForCollation)) {
             throw UnsupportedException::collationNotSupported();
+        }
+
+        if (isset($this->options['writeConcern']) && ! \MongoDB\server_supports_feature($server, self::$wireVersionForWriteConcern)) {
+            throw UnsupportedException::writeConcernNotSupported();
         }
 
         if (\MongoDB\server_supports_feature($server, self::$wireVersionForCommand)) {
@@ -104,12 +123,16 @@ class CreateIndexes implements Executable
      */
     private function executeCommand(Server $server)
     {
-        $command = new Command([
+        $cmd = [
             'createIndexes' => $this->collectionName,
             'indexes' => $this->indexes,
-        ]);
+        ];
 
-        $server->executeCommand($this->databaseName, $command);
+        if (isset($this->options['writeConcern'])) {
+            $cmd['writeConcern'] = \MongoDB\write_concern_as_document($this->options['writeConcern']);
+        }
+
+        $server->executeCommand($this->databaseName,  new Command($cmd));
     }
 
     /**

src/Operation/DropCollection.php
@@ -4,6 +4,7 @@ namespace MongoDB\Operation;
 
 use MongoDB\Driver\Command;
 use MongoDB\Driver\Server;
+use MongoDB\Driver\WriteConcern;
 use MongoDB\Driver\Exception\RuntimeException;
 use MongoDB\Exception\InvalidArgumentException;
 
@@ -18,6 +19,7 @@ use MongoDB\Exception\InvalidArgumentException;
 class DropCollection implements Executable
 {
     private static $errorMessageNamespaceNotFound = 'ns not found';
+    private static $wireVersionForWriteConcern = 5;
 
     private $databaseName;
     private $collectionName;
@@ -31,6 +33,11 @@ class DropCollection implements Executable
      *  * typeMap (array): Type map for BSON deserialization. This will be used
      *    for the returned command result document.
      *
+     *  * writeConcern (MongoDB\Driver\WriteConcern): Write concern.
+     *
+     *    This is not supported for server versions < 3.4 and will result in an
+     *    exception at execution time if used.
+     *
      * @param string $databaseName   Database name
      * @param string $collectionName Collection name
      * @param array  $options        Command options
@@ -39,6 +46,10 @@ class DropCollection implements Executable
     {
         if (isset($options['typeMap']) && ! is_array($options['typeMap'])) {
             throw InvalidArgumentException::invalidType('"typeMap" option', $options['typeMap'], 'array');
+        }
+
+        if (isset($options['writeConcern']) && ! $options['writeConcern'] instanceof WriteConcern) {
+            throw InvalidArgumentException::invalidType('"writeConcern" option', $options['writeConcern'], 'MongoDB\Driver\WriteConcern');
         }
 
         $this->databaseName = (string) $databaseName;
@@ -52,11 +63,16 @@ class DropCollection implements Executable
      * @see Executable::execute()
      * @param Server $server
      * @return array|object Command result document
+     * @throws UnsupportedException if writeConcern is used and unsupported
      */
     public function execute(Server $server)
     {
+        if (isset($this->options['writeConcern']) && ! \MongoDB\server_supports_feature($server, self::$wireVersionForWriteConcern)) {
+            throw UnsupportedException::writeConcernNotSupported();
+        }
+
         try {
-            $cursor = $server->executeCommand($this->databaseName, new Command(['drop' => $this->collectionName]));
+            $cursor = $server->executeCommand($this->databaseName, $this->createCommand());
         } catch (RuntimeException $e) {
             /* The server may return an error if the collection does not exist.
              * Check for an error message (unfortunately, there isn't a code)
@@ -74,5 +90,21 @@ class DropCollection implements Executable
         }
 
         return current($cursor->toArray());
+    }
+
+    /**
+     * Create the drop command.
+     *
+     * @return Command
+     */
+    private function createCommand()
+    {
+        $cmd = ['drop' => $this->collectionName];
+
+        if (isset($this->options['writeConcern'])) {
+            $cmd['writeConcern'] = \MongoDB\write_concern_as_document($this->options['writeConcern']);
+        }
+
+        return new Command($cmd);
     }
 }

commit eee864a90cf31ec87f9a795fa7b93f78599ce21b

Ensure Client's $writeConcern property is initialized
Fixes a bug introduced in 21b7d92aef4def00abade10911bae41f1b5ea378 for PHPLIB-200

src/Client.php
@@ -25,6 +25,7 @@ class Client
     private $manager;
     private $uri;
     private $typeMap;
+    private $writeConcern;
 
     /**
      * Constructs a new Client instance.
@@ -63,6 +64,7 @@ class Client
         unset($driverOptions['typeMap']);
 
         $this->manager = new Manager($uri, $uriOptions, $driverOptions);
+        $this->writeConcern = $this->manager->getWriteConcern();
     }
 
     /**
@@ -77,6 +79,7 @@ class Client
             'manager' => $this->manager,
             'uri' => $this->uri,
             'typeMap' => $this->typeMap,
+            'writeConcern' => $this->writeConcern,
         ];
     }
 
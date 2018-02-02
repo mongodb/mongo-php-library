78e4db1ed230b1b29fb12c59e94faf9c74a4e859
Jeremy Mikola <jmikola@gmail.com>
2018-02-02 10:39:22 -0500

    Consolidate TypeMapArrayIterator bad method tests

--- tests/Model/TypeMapArrayIteratorTest.php
+++ tests/Model/TypeMapArrayIteratorTest.php
@@ -85,14 +85,14 @@ class TypeMapArrayIteratorTest extends TestCase
         $iterator->rewind();
 
         $iterator->asort();
-
     }
 
     /**
+     * @dataProvider provideMutateMethods
      * @expectedException MongoDB\Exception\BadMethodCallException
      * @expectedExceptionMessage MongoDB\Model\TypeMapArrayIterator is immutable
      */
-    public function testAsortThrowsException()
+    public function testMutateMethodsCannotBeCalled($method, $args)
     {
         $document = [
             'array' => [1, 2, 3],
@@ -107,220 +107,23 @@ class TypeMapArrayIteratorTest extends TestCase
 
         $iterator = new TypeMapArrayIterator([$document], $typeMap);
 
-        $expectedDocument = (object) [
-            'array' => [1, 2, 3],
-            'object' => (object) ['foo' => 'bar'],
-        ];
-
         $iterator->rewind();
 
-        $iterator->asort();
+        call_user_func_array([$iterator, $method], $args);
     }
 
-    /**
-     * @expectedException MongoDB\Exception\BadMethodCallException
-     * @expectedExceptionMessage MongoDB\Model\TypeMapArrayIterator is immutable
-     */
-    public function testKsortThrowsException()
+    public function provideMutateMethods()
     {
-        $document = [
-            'array' => [1, 2, 3],
-            'object' => ['foo' => 'bar'],
+        return [
+            ['append', [['x' => 1]]],
+            ['asort', []],
+            ['ksort', []],
+            ['natcasesort', []],
+            ['natsort', []],
+            ['offsetSet', [0, ['x' => 1]]],
+            ['offsetUnset', [0]],
+            ['uasort', [function($a, $b) { return 0; }]],
+            ['uksort', [function($a, $b) { return 0; }]],
         ];
-
-        $typeMap = [
-            'root' => 'object',
-            'document' => 'object',
-            'array' => 'array',
-        ];
-
-        $iterator = new TypeMapArrayIterator([$document], $typeMap);
-
-        $expectedDocument = (object) [
-            'array' => [1, 2, 3],
-            'object' => (object) ['foo' => 'bar'],
-        ];
-
-        $iterator->rewind();
-
-        $iterator->ksort();
-    }
-
-    /**
-     * @expectedException MongoDB\Exception\BadMethodCallException
-     * @expectedExceptionMessage MongoDB\Model\TypeMapArrayIterator is immutable
-     */
-    public function testNatcasessortThrowsException()
-    {
-        $document = [
-            'array' => [1, 2, 3],
-            'object' => ['foo' => 'bar'],
-        ];
-
-        $typeMap = [
-            'root' => 'object',
-            'document' => 'object',
-            'array' => 'array',
-        ];
-
-        $iterator = new TypeMapArrayIterator([$document], $typeMap);
-
-        $expectedDocument = (object) [
-            'array' => [1, 2, 3],
-            'object' => (object) ['foo' => 'bar'],
-        ];
-
-        $iterator->rewind();
-
-        $iterator->natcasesort();
-    }
-
-    /**
-     * @expectedException MongoDB\Exception\BadMethodCallException
-     * @expectedExceptionMessage MongoDB\Model\TypeMapArrayIterator is immutable
-     */
-    public function testNatsortThrowsException()
-    {
-        $document = [
-            'array' => [1, 2, 3],
-            'object' => ['foo' => 'bar'],
-        ];
-
-        $typeMap = [
-            'root' => 'object',
-            'document' => 'object',
-            'array' => 'array',
-        ];
-
-        $iterator = new TypeMapArrayIterator([$document], $typeMap);
-
-        $expectedDocument = (object) [
-            'array' => [1, 2, 3],
-            'object' => (object) ['foo' => 'bar'],
-        ];
-
-        $iterator->rewind();
-
-        $iterator->natsort();
-    }
-
-    /**
-     * @expectedException MongoDB\Exception\BadMethodCallException
-     * @expectedExceptionMessage MongoDB\Model\TypeMapArrayIterator is immutable
-     */
-    public function testOffsetSetThrowsException()
-    {
-        $document = [
-            'array' => [1, 2, 3],
-            'object' => ['foo' => 'bar'],
-        ];
-
-        $typeMap = [
-            'root' => 'object',
-            'document' => 'object',
-            'array' => 'array',
-        ];
-
-        $iterator = new TypeMapArrayIterator([$document], $typeMap);
-
-        $expectedDocument = (object) [
-            'array' => [1, 2, 3],
-            'object' => (object) ['foo' => 'bar'],
-        ];
-
-        $iterator->rewind();
-
-        $iterator->offsetSet(0, 3);
-    }
-
-    /**
-     * @expectedException MongoDB\Exception\BadMethodCallException
-     * @expectedExceptionMessage MongoDB\Model\TypeMapArrayIterator is immutable
-     */
-    public function testOffsetUnsetThrowsException()
-    {
-        $document = [
-            'array' => [1, 2, 3],
-            'object' => ['foo' => 'bar'],
-        ];
-
-        $typeMap = [
-            'root' => 'object',
-            'document' => 'object',
-            'array' => 'array',
-        ];
-
-        $iterator = new TypeMapArrayIterator([$document], $typeMap);
-
-        $expectedDocument = (object) [
-            'array' => [1, 2, 3],
-            'object' => (object) ['foo' => 'bar'],
-        ];
-
-        $iterator->rewind();
-
-        $iterator->offsetUnset(0);
-    }
-
-    /**
-     * @expectedException MongoDB\Exception\BadMethodCallException
-     * @expectedExceptionMessage MongoDB\Model\TypeMapArrayIterator is immutable
-     */
-    public function testUasortThrowsException()
-    {
-        $document = [
-            'array' => [1, 2, 3],
-            'object' => ['foo' => 'bar'],
-        ];
-
-        $typeMap = [
-            'root' => 'object',
-            'document' => 'object',
-            'array' => 'array',
-        ];
-
-        $iterator = new TypeMapArrayIterator([$document], $typeMap);
-
-        $expectedDocument = (object) [
-            'array' => [1, 2, 3],
-            'object' => (object) ['foo' => 'bar'],
-        ];
-
-        $iterator->rewind();
-        $cmp_function = function($a, $b) {
-            return $a;
-        };
-        $iterator->uasort($cmp_function(0, 1));
-    }
-
-    /**
-     * @expectedException MongoDB\Exception\BadMethodCallException
-     * @expectedExceptionMessage MongoDB\Model\TypeMapArrayIterator is immutable
-     */
-    public function testUksortThrowsException()
-    {
-        $document = [
-            'array' => [1, 2, 3],
-            'object' => ['foo' => 'bar'],
-        ];
-
-        $typeMap = [
-            'root' => 'object',
-            'document' => 'object',
-            'array' => 'array',
-        ];
-
-        $iterator = new TypeMapArrayIterator([$document], $typeMap);
-
-        $expectedDocument = (object) [
-            'array' => [1, 2, 3],
-            'object' => (object) ['foo' => 'bar'],
-        ];
-
-        $iterator->rewind();
-        $cmp_function = function($a, $b) {
-            return $a;
-        };
-        $iterator->uksort($cmp_function(0, 1));
     }
 }

980746df7117c939813fd679cb7db2fcc447eea8
Katherine Walker <katherine.walker@mongodb.com>
2018-04-02 12:03:20 -0400

    PHPLIB-226: Implement ArrayAccess for CollectionInfo and DatabaseInfo

--- tests/Model/DatabaseInfoTest.php
+++ tests/Model/DatabaseInfoTest.php
@@ -2,6 +2,7 @@
 
 namespace MongoDB\Tests\Model;
 
+use MongoDB\Exception\BadMethodCallException;
 use MongoDB\Model\DatabaseInfo;
 use MongoDB\Tests\TestCase;
 
@@ -38,5 +39,29 @@ class DatabaseInfoTest extends TestCase
 
         $info = new DatabaseInfo($expectedInfo);
         $this->assertSame($expectedInfo, $info->__debugInfo());
+    }
+
+    public function testImplementsArrayAccess()
+    {
+        $info = new DatabaseInfo(['name' => 'foo']);
+        $this->assertInstanceOf('ArrayAccess', $info);
+        $this->assertArrayHasKey('name', $info);
+        $this->assertSame('foo', $info['name']);
+    }
+
+    public function testOffsetSetCannotBeCalled()
+    {
+        $info = new DatabaseInfo(['name' => 'foo', 'sizeOnDisk' => 1048576, 'empty' => false]);
+        $this->expectException(BadMethodCallException::class);
+        $this->expectExceptionMessage('MongoDB\Model\DatabaseInfo is immutable');
+        $info['empty'] = true;
+    }
+
+    public function testOffsetUnsetCannotBeCalled()
+    {
+        $info = new DatabaseInfo(['name' => 'foo', 'sizeOnDisk' => 1048576, 'empty' => false]);
+        $this->expectException(BadMethodCallException::class);
+        $this->expectExceptionMessage('MongoDB\Model\DatabaseInfo is immutable');
+        unset($info['empty']);
     }
 }
--- tests/Operation/ListCollectionsFunctionalTest.php
+++ tests/Operation/ListCollectionsFunctionalTest.php
@@ -34,6 +34,30 @@ class ListCollectionsFunctionalTest extends FunctionalTestCase
         }
     }
 
+    public function testIdIndexAndInfo()
+    {
+        if (version_compare($this->getServerVersion(), '3.4.0', '<')) {
+            $this->markTestSkipped('idIndex and info are not supported');
+        }
+
+        $server = $this->getPrimaryServer();
+
+        $insertOne = new InsertOne($this->getDatabaseName(), $this->getCollectionName(), ['x' => 1]);
+        $writeResult = $insertOne->execute($server);
+        $this->assertEquals(1, $writeResult->getInsertedCount());
+
+        $operation = new ListCollections($this->getDatabaseName(), ['filter' => ['name' => $this->getCollectionName()]]);
+        $collections = $operation->execute($server);
+
+        $this->assertInstanceOf('MongoDB\Model\CollectionInfoIterator', $collections);
+
+        foreach ($collections as $collection) {
+            $this->assertInstanceOf('MongoDB\Model\CollectionInfo', $collection);
+            $this->assertArrayHasKey('readOnly', $collection['info']);
+            $this->assertEquals(['v' => 2, 'key' => ['_id' => 1], 'name' => '_id_', 'ns' => $this->getNamespace()], $collection['idIndex']);
+        }
+    }
+
     public function testListCollectionsForNonexistentDatabase()
     {
         $server = $this->getPrimaryServer();

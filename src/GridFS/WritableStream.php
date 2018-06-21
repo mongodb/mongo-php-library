f169616c11574e2ea59d2891e5b2d38a7c942fa8
Derick Rethans <github@derickrethans.nl>
2018-06-21 10:55:43 +0100

    PHPLIB-319: Added 'disableMD5' option as GridFS MD5 digest must be optional

--- src/GridFS/WritableStream.php
+++ src/GridFS/WritableStream.php
@@ -36,9 +36,10 @@ class WritableStream
     private $buffer = '';
     private $chunkOffset = 0;
     private $chunkSize;
+    private $disableMD5;
     private $collectionWrapper;
-    private $ctx;
     private $file;
+    private $hashCtx;
     private $isClosed = false;
     private $length = 0;
 
@@ -55,6 +56,9 @@ class WritableStream
      *
      *  * chunkSizeBytes (integer): The chunk size in bytes. Defaults to
      *    261120 (i.e. 255 KiB).
+     *
+     *  * disableMD5 (boolean): When true, no MD5 sum will be generated.
+     *    Defaults to "false".
      *
      *  * contentType (string): DEPRECATED content type to be stored with the
      *    file. This information should now be added to the metadata.
@@ -86,6 +90,10 @@ class WritableStream
             throw new InvalidArgumentException(sprintf('Expected "chunkSizeBytes" option to be >= 1, %d given', $options['chunkSizeBytes']));
         }
 
+        if (isset($options['disableMD5']) && ! is_bool($options['disableMD5'])) {
+            throw InvalidArgumentException::invalidType('"disableMD5" option', $options['disableMD5'], 'boolean');
+        }
+
         if (isset($options['contentType']) && ! is_string($options['contentType'])) {
             throw InvalidArgumentException::invalidType('"contentType" option', $options['contentType'], 'string');
         }
@@ -96,7 +104,11 @@ class WritableStream
 
         $this->chunkSize = $options['chunkSizeBytes'];
         $this->collectionWrapper = $collectionWrapper;
-        $this->ctx = hash_init('md5');
+        $this->disableMD5 = isset($options['disableMD5']) ? $options['disableMD5'] : false;
+
+        if ( ! $this->disableMD5) {
+            $this->hashCtx = hash_init('md5');
+        }
 
         $this->file = [
             '_id' => $options['_id'],
@@ -219,11 +231,13 @@ class WritableStream
 
     private function fileCollectionInsert()
     {
-        $md5 = hash_final($this->ctx);
-
         $this->file['length'] = $this->length;
-        $this->file['md5'] = $md5;
         $this->file['uploadDate'] = new UTCDateTime;
+
+        if ( ! $this->disableMD5) {
+            $md5 = hash_final($this->hashCtx);
+            $this->file['md5'] = $md5;
+        }
 
         try {
             $this->collectionWrapper->insertFile($this->file);
@@ -251,7 +265,9 @@ class WritableStream
             'data' => new Binary($data, Binary::TYPE_GENERIC),
         ];
 
-        hash_update($this->ctx, $data);
+        if ( ! $this->disableMD5) {
+            hash_update($this->hashCtx, $data);
+        }
 
         try {
             $this->collectionWrapper->insertChunk($chunk);

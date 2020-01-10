672c34ed4ef0c87799873a553a7e942756004800
Andreas Braun <git@alcaeus.org>
2020-01-10 13:35:09 +0100

    PHPLIB-519: Test against upcoming driver versions

--- .travis/install-extension.sh
+++ .travis/install-extension.sh
@@ -16,7 +16,22 @@ tpecl () {
     fi
 }
 
-if [ "x${DRIVER_VERSION}" != "x" ]; then
+if [ "x${DRIVER_BRANCH}" != "x" ]; then
+  echo "Compiling driver branch ${DRIVER_BRANCH}"
+
+  mkdir -p /tmp/compile
+  git clone https://github.com/mongodb/mongo-php-driver /tmp/compile/mongo-php-driver
+  cd /tmp/compile/mongo-php-driver
+
+  git checkout ${DRIVER_BRANCH}
+  git submodule update --init
+  phpize
+  ./configure --enable-mongodb-developer-flags
+  make all -j20 > /dev/null
+  make install
+
+  echo "extension=mongodb.so" >> `php --ini | grep "Scan for additional .ini files in" | sed -e "s|.*:\s*||"`/mongodb.ini
+elif [ "x${DRIVER_VERSION}" != "x" ]; then
   echo "Installing driver version ${DRIVER_VERSION} from PECL"
   tpecl mongodb-${DRIVER_VERSION} mongodb.so
 fi

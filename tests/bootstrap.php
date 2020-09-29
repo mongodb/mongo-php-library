a9f80c7e4c7033caaaaaf6d8f001deba5e841dfb
Jeremy Mikola <jmikola@gmail.com>
2020-09-29 17:43:39 +0800

    Include PHPUnit functions via autoload-dev

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,10 +10,6 @@ if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
     throw new Exception('Can\'t find autoload.php. Did you install dependencies with Composer?');
 }
 
-/* Manually include assertion functions for PHPUnit 8.x and earlier.
- * See: https://github.com/sebastianbergmann/phpunit/issues/3746 */
-require_once(__DIR__ . '/../vendor/phpunit/phpunit/src/Framework/Assert/Functions.php');
-
 if (! class_exists(PHPUnit\Framework\Error\Warning::class)) {
     class_alias(PHPUnit_Framework_Error_Warning::class, PHPUnit\Framework\Error\Warning::class);
 }

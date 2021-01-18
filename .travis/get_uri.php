ab0a083f00f35ca9b829b225d1ce2838839adf64
Andreas Braun <git@alcaeus.org>
2021-01-18 12:43:54 +0100

    Remove travis-ci configs

--- .travis/get_uri.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$uriField = isset($argv[1]) ? $argv[1] : 'mongodb_uri';
-$uriSuffix = isset($argv[2]) ? $argv[2] : '';
-
-echo json_decode(file_get_contents("php://stdin"))->$uriField, $uriSuffix;

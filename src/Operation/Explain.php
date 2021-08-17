0142530de14b48792e63e284646c8bb3635798a9
Alexander Golin <agolin95@gmail.com>
2021-08-17 15:52:47 -0400

    standardize copyright formatting

--- src/Operation/Explain.php
+++ src/Operation/Explain.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Copyright 2018 MongoDB, Inc.
+ * Copyright 2018-present MongoDB, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.

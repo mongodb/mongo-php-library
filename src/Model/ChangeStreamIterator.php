4a14c76e7c52ca9efef65bf1c80d9fe598c35d31
Alexander Golin <agolin95@gmail.com>
2021-08-17 16:38:39 -0400

    PHPLIB-692: Standardize copyright formatting (#856)

--- src/Model/ChangeStreamIterator.php
+++ src/Model/ChangeStreamIterator.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Copyright 2019 MongoDB, Inc.
+ * Copyright 2019-present MongoDB, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.

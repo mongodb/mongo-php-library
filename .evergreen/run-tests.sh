4fd4791e729b32023e6c0a7754a2ce605a1d7b5d
Jeremy Mikola <jmikola@gmail.com>
2022-08-31 14:32:19 -0400

    PHPLIB-911: Test with consistent version of crypt_shared
    Use the crypt_shared library provisioned by download-mongodb.sh and add an additional task to disable the library and fall back to mongocryptd.
    
    This also revises how we pass environment variables into run-tests.sh and the test suite, and restructures run-tests.sh a bit.

--- .evergreen/run-tests.sh
+++ .evergreen/run-tests.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 set -o errexit  # Exit the script with error if any of the commands fail
 
-# Supported/used environment variables:
-#   SSL               Set to "yes" to enable SSL. Defaults to "nossl"
-#   MONGODB_URI       Set the suggested connection MONGODB_URI (including credentials and topology info)
-#   API_VERSION       Optional API_VERSION environment variable for run-tests.php
-#   IS_MATRIX_TESTING Set to "true" to enable matrix testing. Defaults to empty string. If "true", DRIVER_MONGODB_VERSION and MONGODB_VERSION will also be checked.
-
-SSL=${SSL:-nossl}
-MONGODB_URI=${MONGODB_URI:-}
-API_VERSION=${API_VERSION:-}
-IS_MATRIX_TESTING=${IS_MATRIX_TESTING:-}
+# Supported environment variables
+API_VERSION=${API_VERSION:-} # Optional API_VERSION environment variable
+CRYPT_SHARED_LIB_PATH="${CRYPT_SHARED_LIB_PATH:-}" # Optional path to crypt_shared library
+DRIVER_MONGODB_VERSION={$DRIVER_MONGODB_VERSION:-} # Required if IS_MATRIX_TESTING is "true"
+IS_MATRIX_TESTING=${IS_MATRIX_TESTING:-} # Specify "true" to enable matrix testing. Defaults to empty string. If "true", DRIVER_MONGODB_VERSION and MONGODB_VERSION will also be checked.
+MONGODB_URI=${MONGODB_URI:-} # Connection string (including credentials and topology info)
+MONGODB_VERSION=${MONGODB_VERSION:-} # Required if IS_MATRIX_TESTING is "true"
+SKIP_CRYPT_SHARED="${SKIP_CRYPT_SHARED:-no}" # Specify "yes" to ignore CRYPT_SHARED_LIB_PATH. Defaults to "no"
+SSL=${SSL:-no} # Specify "yes" to enable SSL. Defaults to "no"
+TESTS=${TESTS:-} # Optional test group. Defaults to all tests
 
 # For matrix testing, we have to determine the correct driver version
 if [ "${IS_MATRIX_TESTING}" = "true" ]; then
@@ -42,7 +42,7 @@ if [ "${IS_MATRIX_TESTING}" = "true" ]; then
 fi
 
 # Enable verbose output to see skipped and incomplete tests
-PHPUNIT_OPTS="${PHPUNIT_OPTS} -v"
+PHPUNIT_OPTS="${PHPUNIT_OPTS} -v --configuration phpunit.evergreen.xml"
 
 # Determine if MONGODB_URI already has a query string
 SUFFIX=$(echo "$MONGODB_URI" | grep -Eo "\?(.*)" | cat)
@@ -57,29 +57,34 @@ fi
 
 echo "Running tests with URI: $MONGODB_URI"
 
-# Disable failing PHPUnit due to deprecations
+# Disable PHPUnit test failures due to deprecations
+# See: https://symfony.com/doc/current/components/phpunit_bridge.html#internal-deprecations
 export SYMFONY_DEPRECATIONS_HELPER=999999
 
-# Run the tests, and store the results in a Evergreen compatible JSON results file
+# Export environment vars that may be referenced by the test suite
+export API_VERSION="${API_VERSION}"
+export CRYPT_SHARED_LIB_PATH="${CRYPT_SHARED_LIB_PATH}"
+export MONGODB_URI="${MONGODB_URI}"
+
+# Run the tests, and store the results in a junit result file
 case "$TESTS" in
    atlas-data-lake*)
-      MONGODB_URI="mongodb://mhuser:pencil@127.0.0.1:27017"
-      php vendor/bin/simple-phpunit --configuration phpunit.evergreen.xml --testsuite "Atlas Data Lake Test Suite" $PHPUNIT_OPTS
+      php vendor/bin/simple-phpunit $PHPUNIT_OPTS --testsuite "Atlas Data Lake Test Suite"
       ;;
 
    csfle)
-      php vendor/bin/simple-phpunit --configuration phpunit.evergreen.xml --group csfle $PHPUNIT_OPTS
+      php vendor/bin/simple-phpunit $PHPUNIT_OPTS --group csfle
       ;;
 
    versioned-api)
-      php vendor/bin/simple-phpunit --configuration phpunit.evergreen.xml --group versioned-api $PHPUNIT_OPTS
+      php vendor/bin/simple-phpunit $PHPUNIT_OPTS --group versioned-api
       ;;
 
    serverless)
-      php vendor/bin/simple-phpunit --configuration phpunit.evergreen.xml --group serverless $PHPUNIT_OPTS
+      php vendor/bin/simple-phpunit $PHPUNIT_OPTS --group serverless
       ;;
 
    *)
-      php vendor/bin/simple-phpunit --configuration phpunit.evergreen.xml $PHPUNIT_OPTS
+      php vendor/bin/simple-phpunit $PHPUNIT_OPTS
       ;;
 esac

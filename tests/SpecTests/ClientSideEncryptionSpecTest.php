07b48fc6e4d6af0b0170081caea568086f2ad1fa
Jeremy Mikola <jmikola@gmail.com>
2022-09-14 15:22:58 -0400

    PHPLIB-947: Skip legacy CSFLE explain test pending crypt_shared fix (#972)

--- tests/SpecTests/ClientSideEncryptionSpecTest.php
+++ tests/SpecTests/ClientSideEncryptionSpecTest.php
@@ -65,6 +65,7 @@ class ClientSideEncryptionSpecTest extends FunctionalTestCase
         'awsTemporary: Insert a document with auto encryption using the AWS provider with temporary credentials' => 'Not yet implemented (PHPC-1751)',
         'awsTemporary: Insert with invalid temporary credentials' => 'Not yet implemented (PHPC-1751)',
         'azureKMS: Insert a document with auto encryption using Azure KMS provider' => 'RHEL platform is missing Azure root certificate (PHPLIB-619)',
+        'explain: Explain a find with deterministic encryption' => 'crypt_shared does not add apiVersion field to explain commands (PHPLIB-947, SERVER-69564)',
         'timeoutMS: timeoutMS applied to listCollections to get collection schema' => 'Not yet implemented (PHPC-1760)',
         'timeoutMS: remaining timeoutMS applied to find to get keyvault data' => 'Not yet implemented (PHPC-1760)',
     ];

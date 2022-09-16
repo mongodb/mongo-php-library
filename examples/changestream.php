153f61d0af7e521c317697bfa3aa82759965a6ce
Jeremy Mikola <jmikola@gmail.com>
2022-09-16 14:35:16 -0400

    PHPLIB-920: Command monitoring and change stream examples (#976)

---
+++ examples/changestream.php
@@ -0,0 +1,58 @@
+<?php
+declare(strict_types=1);
+
+namespace MongoDB\Examples;
+
+require '../vendor/autoload.php';
+
+use MongoDB\Client;
+
+use function assert;
+use function fprintf;
+use function getenv;
+use function is_object;
+use function MongoDB\BSON\fromPHP;
+use function MongoDB\BSON\toRelaxedExtendedJSON;
+use function printf;
+use function time;
+
+use const STDERR;
+
+function toJSON(object $document): string
+{
+    return toRelaxedExtendedJSON(fromPHP($document));
+}
+
+$client = new Client(getenv('MONGODB_URI') ?: 'mongodb://127.0.0.1/');
+
+$collection = $client->test->coll;
+$collection->drop();
+
+$changeStream = $collection->watch();
+
+$documents = [];
+
+for ($i = 0; $i < 10; $i++) {
+    $documents[] = ['x' => $i];
+}
+
+$collection->insertMany($documents);
+
+$changeStream->rewind();
+
+$startTime = time();
+
+while (true) {
+    if ($changeStream->valid()) {
+        $event = $changeStream->current();
+        assert(is_object($event));
+        printf("%s\n", toJSON($event));
+    }
+
+    $changeStream->next();
+
+    if (time() - $startTime > 3) {
+        fprintf(STDERR, "Aborting after 3 seconds...\n");
+        break;
+    }
+}

9aeb3b080d54ba641e0bb55f49234692c86ed7f7
Andreas Braun <git@alcaeus.org>
2022-09-28 15:24:42 +0200

    Add example for deserialisation using typemap

---
+++ examples/typemap.php
@@ -0,0 +1,121 @@
+<?php
+declare(strict_types=1);
+
+namespace MongoDB\Examples;
+
+use MongoDB\BSON\ObjectId;
+use MongoDB\BSON\Unserializable;
+use MongoDB\Client;
+use UnexpectedValueException;
+
+use function dirname;
+use function getenv;
+use function is_array;
+use function var_dump;
+
+require dirname(__FILE__) . '/../vendor/autoload.php';
+
+class TypemapEntry implements Unserializable
+{
+    /** @var ObjectId */
+    private $id;
+
+    /** @var string */
+    private $name;
+
+    /** @var array<TypemapEmail> */
+    private $emails;
+
+    private function __construct()
+    {
+    }
+
+    public function getId(): ObjectId
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getEmails(): array
+    {
+        return $this->emails;
+    }
+
+    public function bsonUnserialize(array $data): void
+    {
+        if (! $data['_id'] instanceof ObjectId) {
+            throw new UnexpectedValueException('_id field is not of the expected type');
+        }
+
+        if (! is_array($data['emails'])) {
+            throw new UnexpectedValueException('emails field is not of the expected type');
+        }
+
+        $this->id = $data['_id'];
+        $this->name = (string) $data['name'];
+
+        /** @psalm-suppress MixedPropertyTypeCoercion */
+        $this->emails = $data['emails'];
+    }
+}
+
+class TypemapEmail implements Unserializable
+{
+    /** @var string */
+    private $type;
+
+    /** @var string */
+    private $address;
+
+    private function __construct()
+    {
+    }
+
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    public function getAddress(): string
+    {
+        return $this->address;
+    }
+
+    public function bsonUnserialize(array $data): void
+    {
+        $this->type = (string) $data['type'];
+        $this->address = (string) $data['address'];
+    }
+}
+
+$client = new Client(getenv('MONGODB_URI') ?: 'mongodb://127.0.0.1/');
+
+$collection = $client->test->coll;
+$collection->drop();
+
+$document = [
+    'name' => 'alcaeus',
+    'emails' => [
+        ['type' => 'work', 'address' => 'alcaeus@example.com'],
+        ['type' => 'private', 'address' => 'secret@example.com'],
+    ],
+];
+
+$collection->insertOne($document);
+
+$typeMap = [
+    'root' => TypemapEntry::class, // Root object will be an Entry instance
+    'fieldPaths' => [
+        'emails' => 'array', // Emails field is used as PHP array
+        'emails.$' => TypemapEmail::class, // Each element in the emails array will be an Email instance
+    ],
+];
+
+$entry = $collection->findOne([], ['typeMap' => $typeMap]);
+
+/** @psalm-suppress ForbiddenCode */
+var_dump($entry);

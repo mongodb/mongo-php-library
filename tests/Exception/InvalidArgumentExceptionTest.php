038977d90615bc68bffb3fe5d18a675288c725ea
Jeremy Mikola <jmikola@gmail.com>
2023-03-18 21:28:35 +0800

    PHPLIB-1093: Assert $expectedType array is non-empty for InvalidArgumentException
    This also factors out the logic for converting an $expectedType array to a string.

---
+++ tests/Exception/InvalidArgumentExceptionTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace MongoDB\Tests\Exception;
+
+use MongoDB\Exception\InvalidArgumentException;
+use MongoDB\Tests\TestCase;
+
+use AssertionError;
+
+class InvalidArgumentExceptionTest extends TestCase
+{
+    /**
+     * @dataProvider provideExpectedTypes
+     */
+    public function testExpectedTypeFormatting($expectedType, $typeString): void
+    {
+        $e = InvalidArgumentException::invalidType('$arg', null, $expectedType);
+        $this->assertStringContainsString($typeString, $e->getMessage());
+    }
+
+    public function provideExpectedTypes()
+    {
+        yield 'expectedType is a string' => [
+            'array',
+            'type "array"',
+        ];
+
+        yield 'expectedType is an array with one string' => [
+            ['array'],
+            'type "array"',
+        ];
+
+        yield 'expectedType is an array with two strings' => [
+            ['array', 'integer'],
+            'type "array" or "integer"',
+        ];
+
+        yield 'expectedType is an array with three strings' => [
+            ['array', 'integer', 'object'],
+            'type "array", "integer", or "object"',
+        ];
+    }
+
+    public function testExpectedTypeArrayMustNotBeEmpty(): void
+    {
+        $this->expectException(AssertionError::class);
+        InvalidArgumentException::invalidType('$arg', null, []);
+    }
+}

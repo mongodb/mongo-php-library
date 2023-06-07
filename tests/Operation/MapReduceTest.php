3c4e101ecbb7312047fd234910990a8f322f3908
Jeremy Mikola <jmikola@gmail.com>
2023-06-06 22:42:21 -0400

    PHPLIB-1122: Support Document for MapReduce $out parameter
    Also adds tests for $out deprecations (re: PHPLIB-480) using various document types.

--- tests/Operation/MapReduceTest.php
+++ tests/Operation/MapReduceTest.php
@@ -2,9 +2,11 @@
 
 namespace MongoDB\Tests\Operation;
 
+use MongoDB\BSON\Document;
 use MongoDB\BSON\Javascript;
 use MongoDB\BSON\ObjectId;
 use MongoDB\Exception\InvalidArgumentException;
+use MongoDB\Model\BSONDocument;
 use MongoDB\Operation\MapReduce;
 use stdClass;
 
@@ -23,6 +25,31 @@ class MapReduceTest extends TestCase
     public function provideInvalidOutValues()
     {
         return $this->wrapValuesForDataProvider([123, 3.14, true]);
+    }
+
+    /** @dataProvider provideDeprecatedOutValues */
+    public function testConstructorOutArgumentDeprecations($out): void
+    {
+        $map = new Javascript('function() { emit(this.x, this.y); }');
+        $reduce = new Javascript('function(key, values) { return Array.sum(values); }');
+
+        $this->assertDeprecated(function () use ($map, $reduce, $out): void {
+            new MapReduce($this->getDatabaseName(), $this->getCollectionName(), $map, $reduce, $out);
+        });
+    }
+
+    public function provideDeprecatedOutValues(): array
+    {
+        return [
+            'nonAtomic:array' => [['nonAtomic' => false]],
+            'nonAtomic:object' => [(object) ['nonAtomic' => false]],
+            'nonAtomic:Serializable' => [new BSONDocument(['nonAtomic' => false])],
+            'nonAtomic:Document' => [Document::fromPHP(['nonAtomic' => false])],
+            'sharded:array' => [['sharded' => false]],
+            'sharded:object' => [(object) ['sharded' => false]],
+            'sharded:Serializable' => [new BSONDocument(['sharded' => false])],
+            'sharded:Document' => [Document::fromPHP(['sharded' => false])],
+        ];
     }
 
     /** @dataProvider provideInvalidConstructorOptions */

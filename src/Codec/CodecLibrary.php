8e818a39c496620ad2c220ee51e0ca9e8fd0115c
Andreas Braun <git@alcaeus.org>
2023-07-06 11:23:38 +0200

    Introduce codec library

---
+++ src/Codec/CodecLibrary.php
@@ -0,0 +1,149 @@
+<?php
+/*
+ * Copyright 2023-present MongoDB, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace MongoDB\Codec;
+
+use MongoDB\Exception\InvalidArgumentException;
+use MongoDB\Exception\UnexpectedValueException;
+
+use function get_debug_type;
+use function sprintf;
+
+class CodecLibrary implements Codec
+{
+    use DecodeIfSupported;
+    use EncodeIfSupported;
+
+    /** @var array<Decoder> */
+    private $decoders = [];
+
+    /** @var array<Encoder> */
+    private $encoders = [];
+
+    /** @param Decoder|Encoder $items */
+    public function __construct(...$items)
+    {
+        foreach ($items as $item) {
+            if (! $item instanceof Decoder && ! $item instanceof Encoder) {
+                throw InvalidArgumentException::invalidType('$items', $item, [Decoder::class, Encoder::class]);
+            }
+
+            if ($item instanceof Codec) {
+                // Use attachCodec to avoid multiple calls to attachLibrary
+                $this->attachCodec($item);
+
+                continue;
+            }
+
+            if ($item instanceof Decoder) {
+                $this->attachDecoder($item);
+            }
+
+            if ($item instanceof Encoder) {
+                $this->attachEncoder($item);
+            }
+        }
+    }
+
+    /** @return static */
+    final public function attachCodec(Codec $codec): self
+    {
+        $this->decoders[] = $codec;
+        $this->encoders[] = $codec;
+        if ($codec instanceof KnowsCodecLibrary) {
+            $codec->attachLibrary($this);
+        }
+
+        return $this;
+    }
+
+    /** @return static */
+    final public function attachDecoder(Decoder $decoder): self
+    {
+        $this->decoders[] = $decoder;
+        if ($decoder instanceof KnowsCodecLibrary) {
+            $decoder->attachLibrary($this);
+        }
+
+        return $this;
+    }
+
+    /** @return static */
+    final public function attachEncoder(Encoder $encoder): self
+    {
+        $this->encoders[] = $encoder;
+        if ($encoder instanceof KnowsCodecLibrary) {
+            $encoder->attachLibrary($this);
+        }
+
+        return $this;
+    }
+
+    /** @param mixed $value */
+    final public function canDecode($value): bool
+    {
+        foreach ($this->decoders as $decoder) {
+            if ($decoder->canDecode($value)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /** @param mixed $value */
+    final public function canEncode($value): bool
+    {
+        foreach ($this->encoders as $encoder) {
+            if ($encoder->canEncode($value)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * @param mixed $value
+     * @return mixed
+     */
+    final public function decode($value)
+    {
+        foreach ($this->decoders as $decoder) {
+            if ($decoder->canDecode($value)) {
+                return $decoder->decode($value);
+            }
+        }
+
+        throw new UnexpectedValueException(sprintf('No decoder found for value of type "%s"', get_debug_type($value)));
+    }
+
+    /**
+     * @param mixed $value
+     * @return mixed
+     */
+    final public function encode($value)
+    {
+        foreach ($this->encoders as $encoder) {
+            if ($encoder->canEncode($value)) {
+                return $encoder->encode($value);
+            }
+        }
+
+        throw new UnexpectedValueException(sprintf('No encoder found for value of type "%s"', get_debug_type($value)));
+    }
+}
---
+++ tests/Codec/CodecLibraryTest.php
@@ -0,0 +1,174 @@
+<?php
+
+namespace MongoDB\Tests\Codec;
+
+use MongoDB\Codec\Codec;
+use MongoDB\Codec\CodecLibrary;
+use MongoDB\Codec\DecodeIfSupported;
+use MongoDB\Codec\EncodeIfSupported;
+use MongoDB\Codec\KnowsCodecLibrary;
+use MongoDB\Exception\UnexpectedValueException;
+use MongoDB\Tests\TestCase;
+
+class CodecLibraryTest extends TestCase
+{
+    public function testDecode(): void
+    {
+        $codec = $this->getCodecLibrary();
+
+        $this->assertTrue($codec->canDecode('cigam'));
+        $this->assertFalse($codec->canDecode('magic'));
+
+        $this->assertSame('magic', $codec->decode('cigam'));
+    }
+
+    public function testDecodeIfSupported(): void
+    {
+        $codec = $this->getCodecLibrary();
+
+        $this->assertSame('magic', $codec->decodeIfSupported('cigam'));
+        $this->assertSame('magic', $codec->decodeIfSupported('magic'));
+    }
+
+    public function testDecodeNull(): void
+    {
+        $codec = $this->getCodecLibrary();
+
+        $this->assertFalse($codec->canDecode(null));
+
+        $this->expectException(UnexpectedValueException::class);
+        $this->expectExceptionMessage('No decoder found for value of type "null"');
+
+        $this->assertNull($codec->decode(null));
+    }
+
+    public function testDecodeUnsupportedValue(): void
+    {
+        $this->expectException(UnexpectedValueException::class);
+        $this->expectExceptionMessage('No decoder found for value of type "string"');
+
+        $this->getCodecLibrary()->decode('foo');
+    }
+
+    public function testEncode(): void
+    {
+        $codec = $this->getCodecLibrary();
+
+        $this->assertTrue($codec->canEncode('magic'));
+        $this->assertFalse($codec->canEncode('cigam'));
+
+        $this->assertSame('cigam', $codec->encode('magic'));
+    }
+
+    public function testEncodeIfSupported(): void
+    {
+        $codec = $this->getCodecLibrary();
+
+        $this->assertSame('cigam', $codec->encodeIfSupported('magic'));
+        $this->assertSame('cigam', $codec->encodeIfSupported('cigam'));
+    }
+
+    public function testEncodeNull(): void
+    {
+        $codec = $this->getCodecLibrary();
+
+        $this->assertFalse($codec->canEncode(null));
+
+        $this->expectException(UnexpectedValueException::class);
+        $this->expectExceptionMessage('No encoder found for value of type "null"');
+
+        $codec->encode(null);
+    }
+
+    public function testEncodeUnsupportedValue(): void
+    {
+        $this->expectException(UnexpectedValueException::class);
+        $this->expectExceptionMessage('No encoder found for value of type "string"');
+
+        $this->getCodecLibrary()->encode('foo');
+    }
+
+    public function testLibraryAttachesToCodecs(): void
+    {
+        $codec = $this->getTestCodec();
+        $library = $this->getCodecLibrary();
+
+        $library->attachCodec($codec);
+        $this->assertSame($library, $codec->library);
+    }
+
+    public function testLibraryAttachesToCodecsWhenCreating(): void
+    {
+        $codec = $this->getTestCodec();
+        $library = new CodecLibrary($codec);
+
+        $this->assertSame($library, $codec->library);
+    }
+
+    private function getCodecLibrary(): CodecLibrary
+    {
+        return new CodecLibrary(
+            /** @template-implements Codec<string, string> */
+            new class implements Codec
+            {
+                use DecodeIfSupported;
+                use EncodeIfSupported;
+
+                public function canDecode($value): bool
+                {
+                    return $value === 'cigam';
+                }
+
+                public function canEncode($value): bool
+                {
+                    return $value === 'magic';
+                }
+
+                public function decode($value)
+                {
+                    return 'magic';
+                }
+
+                public function encode($value)
+                {
+                    return 'cigam';
+                }
+            }
+        );
+    }
+
+    private function getTestCodec(): Codec
+    {
+        return new class implements Codec, KnowsCodecLibrary {
+            use DecodeIfSupported;
+            use EncodeIfSupported;
+
+            public $library;
+
+            public function attachLibrary(CodecLibrary $library): void
+            {
+                $this->library = $library;
+            }
+
+            public function canDecode($value): bool
+            {
+                return false;
+            }
+
+            public function canEncode($value): bool
+            {
+                return false;
+            }
+
+            public function decode($value)
+            {
+                return null;
+            }
+
+            public function encode($value)
+            {
+                return null;
+            }
+        };
+    }
+}

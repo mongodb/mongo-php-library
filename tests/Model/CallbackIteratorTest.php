3bac4027421451f48f31830485f2ab6cbb5ec55e
Andreas Braun <git@alcaeus.org>
2023-07-07 13:46:01 +0200

    Add tests for CallbackIterator

---
+++ tests/Model/CallbackIteratorTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace MongoDB\Tests\Model;
+
+use ArrayIterator;
+use MongoDB\Model\CallbackIterator;
+use MongoDB\Tests\TestCase;
+
+use function array_keys;
+use function iterator_to_array;
+
+class CallbackIteratorTest extends TestCase
+{
+    public function testArrayIteration(): void
+    {
+        $expectedKey = 0;
+
+        $original = [1, 2, 3];
+
+        $callbackIterator = new CallbackIterator(
+            new ArrayIterator($original),
+            function ($value, $key) use (&$expectedKey) {
+                $this->assertSame($expectedKey, $key);
+                $expectedKey++;
+
+                return $value * 2;
+            }
+        );
+
+        $this->assertSame([2, 4, 6], iterator_to_array($callbackIterator));
+    }
+
+    public function testHashIteration(): void
+    {
+        $expectedKey = 0;
+
+        $original = ['a' => 1, 'b' => 2, 'c' => 3];
+        $expectedKeys = array_keys($original);
+
+        $callbackIterator = new CallbackIterator(
+            new ArrayIterator($original),
+            function ($value, $key) use (&$expectedKey, $expectedKeys) {
+                $this->assertSame($expectedKeys[$expectedKey], $key);
+                $expectedKey++;
+
+                return $value * 2;
+            }
+        );
+
+        $this->assertSame(['a' => 2, 'b' => 4, 'c' => 6], iterator_to_array($callbackIterator));
+    }
+}

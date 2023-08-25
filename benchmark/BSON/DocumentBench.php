57a7f3b14814eb9a835208ea3d9ce4e5de21bb31
Andreas Braun <alcaeus@users.noreply.github.com>
2023-08-25 11:06:25 +0200

    PHPLIB-1183: Initial performance tests (#1141)
    * Add basic benchmarks
    
    * Add GitHub actions workflow for performance benchmarks
    
    * Use semicolon for empty foreach
    
    * Remove separate phpbench configuration for CI
    
    * Simplify BSON structure creation
    
    * Remove unnecessary copyright notices
    
    * Ignore phpcs error for inline control structure

---
+++ benchmark/BSON/DocumentBench.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace MongoDB\Benchmark\BSON;
+
+use MongoDB\Benchmark\BaseBench;
+use MongoDB\BSON\Document;
+use PhpBench\Attributes\BeforeMethods;
+
+use function file_get_contents;
+use function iterator_to_array;
+
+#[BeforeMethods('prepareData')]
+final class DocumentBench extends BaseBench
+{
+    private static Document $document;
+
+    public function prepareData(): void
+    {
+        self::$document = Document::fromJSON(file_get_contents(self::LARGE_FILE_PATH));
+    }
+
+    public function benchCheckFirst(): void
+    {
+        self::$document->has('qx3MigjubFSm');
+    }
+
+    public function benchCheckLast(): void
+    {
+        self::$document->has('Zz2MOlCxDhLl');
+    }
+
+    public function benchAccessFirst(): void
+    {
+        self::$document->get('qx3MigjubFSm');
+    }
+
+    public function benchAccessLast(): void
+    {
+        self::$document->get('Zz2MOlCxDhLl');
+    }
+
+    public function benchIteratorToArray(): void
+    {
+        iterator_to_array(self::$document);
+    }
+
+    public function benchToPHPObject(): void
+    {
+        self::$document->toPHP();
+    }
+
+    public function benchToPHPArray(): void
+    {
+        self::$document->toPHP(['root' => 'array']);
+    }
+
+    public function benchIteration(): void
+    {
+        // phpcs:ignore Generic.CodeAnalysis.EmptyStatement.DetectedForeach
+        // phpcs:ignore Generic.ControlStructures.InlineControlStructure.NotAllowed
+        foreach (self::$document as $key => $value);
+    }
+
+    public function benchIterationAsArray(): void
+    {
+        // phpcs:ignore Generic.CodeAnalysis.EmptyStatement.DetectedForeach
+        // phpcs:ignore Generic.ControlStructures.InlineControlStructure.NotAllowed
+        foreach (self::$document->toPHP(['root' => 'array']) as $key => $value);
+    }
+
+    public function benchIterationAsObject(): void
+    {
+        // phpcs:ignore Generic.CodeAnalysis.EmptyStatement.DetectedForeach
+        // phpcs:ignore Generic.ControlStructures.InlineControlStructure.NotAllowed
+        foreach (self::$document->toPHP() as $key => $value);
+    }
+}
---
+++ benchmark/BSON/PackedArrayBench.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace MongoDB\Benchmark\BSON;
+
+use MongoDB\Benchmark\BaseBench;
+use MongoDB\BSON\PackedArray;
+use PhpBench\Attributes\BeforeMethods;
+
+use function array_values;
+use function file_get_contents;
+use function iterator_to_array;
+use function json_decode;
+
+use const JSON_THROW_ON_ERROR;
+
+#[BeforeMethods('prepareData')]
+final class PackedArrayBench extends BaseBench
+{
+    private static PackedArray $array;
+
+    public function prepareData(): void
+    {
+        $array = array_values(json_decode(file_get_contents(self::LARGE_FILE_PATH), true, 512, JSON_THROW_ON_ERROR));
+
+        self::$array = PackedArray::fromPHP($array);
+    }
+
+    public function benchCheckFirst(): void
+    {
+        self::$array->has(0);
+    }
+
+    public function benchCheckLast(): void
+    {
+        self::$array->has(94354);
+    }
+
+    public function benchAccessFirst(): void
+    {
+        self::$array->get(0);
+    }
+
+    public function benchAccessLast(): void
+    {
+        self::$array->get(94354);
+    }
+
+    public function benchIteratorToArray(): void
+    {
+        iterator_to_array(self::$array);
+    }
+
+    public function benchToPHPArray(): void
+    {
+        self::$array->toPHP();
+    }
+
+    public function benchIteration(): void
+    {
+        // phpcs:ignore Generic.CodeAnalysis.EmptyStatement.DetectedForeach
+        // phpcs:ignore Generic.ControlStructures.InlineControlStructure.NotAllowed
+        foreach (self::$array as $key => $value);
+    }
+
+    public function benchIterationAfterIteratorToArray(): void
+    {
+        // phpcs:ignore Generic.CodeAnalysis.EmptyStatement.DetectedForeach
+        // phpcs:ignore Generic.ControlStructures.InlineControlStructure.NotAllowed
+        foreach (iterator_to_array(self::$array) as $key => $value);
+    }
+
+    public function benchIterationAsArray(): void
+    {
+        // phpcs:ignore Generic.CodeAnalysis.EmptyStatement.DetectedForeach
+        // phpcs:ignore Generic.ControlStructures.InlineControlStructure.NotAllowed
+        foreach (self::$array->toPHP() as $key => $value);
+    }
+}

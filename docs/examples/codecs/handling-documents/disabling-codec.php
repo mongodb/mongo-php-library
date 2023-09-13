b7aee6e6674bd013a663f191513d03b1eef9e1de
Andreas Braun <git@alcaeus.org>
2023-09-13 10:10:59 +0200

    Reformat disabling codec examples

--- docs/examples/codecs/handling-documents/disabling-codec.php
+++ docs/examples/codecs/handling-documents/disabling-codec.php
@@ -1,14 +1,7 @@
 <?php
 
 // Returns a document using the default type map
-$collection->findOne(['name' => 'Jane Doe'], [
-    'codec' => null,
-]);
+$collection->findOne($filter, ['codec' => null]);
 
 // Disables codec usage as the aggregate result will have a different format
-$collection->aggregate([['$group' => [
-    '_id' => '$accountType',
-    'count' => ['$sum' => 1],
-]]], [
-    'codec' => null,
-]);
+$collection->aggregate($pipeline, ['codec' => null]);

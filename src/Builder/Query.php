99e22667fd9884bf17956ac031d40129c571f908
Jérôme Tamarelle <jerome@tamarelle.net>
2023-10-11 01:39:24 +0200

    Implement Regex factory

--- src/Builder/Query.php
+++ src/Builder/Query.php
@@ -2,7 +2,29 @@
 
 namespace MongoDB\Builder;
 
+use InvalidArgumentException;
+use MongoDB\BSON\Regex;
+use MongoDB\Builder\Query\RegexOperator;
+
+use function is_string;
+
 enum Query
 {
-    use Query\FactoryTrait;
+    use Query\FactoryTrait {
+        regex as private generatedRegex;
+    }
+
+    /**
+     * Selects documents where values match a specified regular expression.
+     */
+    public static function regex(Regex|string $regex, string $flags = ''): RegexOperator
+    {
+        if (is_string($regex)) {
+            $regex = new Regex($regex, $flags);
+        } elseif ($flags !== '') {
+            throw new InvalidArgumentException('Flags can only be specified when the regex is a string');
+        }
+
+        return self::generatedRegex($regex);
+    }
 }

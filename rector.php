de80302f603e37b8d6ff0b4d67158977237d92a5
Oskar Stark <oskarstark@googlemail.com>
2023-10-21 21:07:57 +0200

    Upgrade `rector/rector`

--- rector.php
+++ rector.php
@@ -2,11 +2,9 @@
 
 use Rector\Config\RectorConfig;
 use Rector\DeadCode\Rector\ClassLike\RemoveAnnotationRector;
-use Rector\Php56\Rector\FunctionLike\AddDefaultValueForUndefinedVariableRector;
-use Rector\Php71\Rector\FuncCall\RemoveExtraParametersRector;
+use Rector\Php70\Rector\StmtsAwareInterface\IfIssetToCoalescingRector;
 use Rector\Php73\Rector\FuncCall\JsonThrowOnErrorRector;
 use Rector\Set\ValueObject\LevelSetList;
-use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromAssignsRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
@@ -20,18 +18,8 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->sets([LevelSetList::UP_TO_PHP_74]);
 
     $rectorConfig->skip([
-        // Falsely detect unassigned variables in code paths stopped by PHPUnit\Framework\Assert::markTestSkipped()
-        AddDefaultValueForUndefinedVariableRector::class => [
-            __DIR__ . '/tests/',
-        ],
-        // @see https://github.com/phpstan/phpstan-src/pull/2429
-        RemoveExtraParametersRector::class => [
-            __DIR__ . '/src/Operation/',
-        ],
-        // Assigns wrong type due to outdated PHPStan stubs
-        TypedPropertyFromAssignsRector::class => [
-            __DIR__ . '/src/Model/BSONIterator.php',
-        ],
+        // Do not use ternaries extensively
+        IfIssetToCoalescingRector::class,
         // Not necessary in documentation examples
         JsonThrowOnErrorRector::class => [
             __DIR__ . '/tests/DocumentationExamplesTest.php',

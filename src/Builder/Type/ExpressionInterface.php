0622bb2c46f15e21cad1a90110d56a7cf09ebf4a
Jérôme Tamarelle <jerome@tamarelle.net>
2024-03-08 10:30:45 +0100

    Fix links

--- src/Builder/Type/ExpressionInterface.php
+++ src/Builder/Type/ExpressionInterface.php
@@ -8,7 +8,7 @@ namespace MongoDB\Builder\Type;
  * Expressions can include field paths, literals, system variables, expression objects, and expression operators.
  * Expressions can be nested.
  *
- * @see https://www.mongodb.com/docs/manual/meta/aggregation-quick-reference/#expressions
+ * @see https://www.mongodb.com/docs/manual/reference/operator/aggregation/
  */
 interface ExpressionInterface
 {
--- src/Builder/Type/StageInterface.php
+++ src/Builder/Type/StageInterface.php
@@ -7,7 +7,7 @@ namespace MongoDB\Builder\Type;
 /**
  * Aggregation Pipeline Stages
  *
- * @see https://www.mongodb.com/docs/manual/meta/aggregation-quick-reference/#stages
+ * @see https://www.mongodb.com/docs/manual/reference/operator/aggregation-pipeline/
  */
 interface StageInterface
 {

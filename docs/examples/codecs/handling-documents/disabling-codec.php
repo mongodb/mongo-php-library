62b2e0e0ea0e31cc1dd248cde42e74a2fbca930c
Heli Aldridge <heli@heli.pet>
2024-08-11 18:32:22 -0400

    DOP-4904: Remove docs/ from v1.17

--- docs/examples/codecs/handling-documents/disabling-codec.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// Overrides the collection codec, falling back to the default type map
-$collection->aggregate($pipeline, ['codec' => null]);
-
-// Overrides the collection codec, using the specified type map
-$collection->findOne($filter, ['typeMap' => ['root' => 'stdClass']]);

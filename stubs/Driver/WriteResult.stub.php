49fa3155ed379447e32c3c8b48d6cd933fcefce2
Jérôme Tamarelle <jerome@tamarelle.net>
2024-09-25 14:45:38 +0200

    Format stubs

--- stubs/Driver/WriteResult.stub.php
+++ stubs/Driver/WriteResult.stub.php
@@ -4,55 +4,29 @@ namespace MongoDB\Driver;
 
 final class WriteResult
 {
-    final private function __construct()
-    {
-    }
+    final private function __construct() {}
 
-    final public function __wakeup(): void
-    {
-    }
+    final public function __wakeup(): void {}
 
-    final public function getDeletedCount(): int
-    {
-    }
+    final public function getDeletedCount(): int {}
 
-    final public function getInsertedCount(): int
-    {
-    }
+    final public function getInsertedCount(): int {}
 
-    final public function getMatchedCount(): int
-    {
-    }
+    final public function getMatchedCount(): int {}
 
-    final public function getModifiedCount(): int
-    {
-    }
+    final public function getModifiedCount(): int {}
 
-    final public function getServer(): Server
-    {
-    }
+    final public function getServer(): Server {}
 
-    final public function getUpsertedCount(): int
-    {
-    }
+    final public function getUpsertedCount(): int {}
 
-    final public function getUpsertedIds(): array
-    {
-    }
+    final public function getUpsertedIds(): array {}
 
-    final public function getWriteConcernError(): ?WriteConcernError
-    {
-    }
+    final public function getWriteConcernError(): ?WriteConcernError {}
 
-    final public function getWriteErrors(): array
-    {
-    }
+    final public function getWriteErrors(): array {}
 
-    final public function getErrorReplies(): array
-    {
-    }
+    final public function getErrorReplies(): array {}
 
-    final public function isAcknowledged(): bool
-    {
-    }
+    final public function isAcknowledged(): bool {}
 }

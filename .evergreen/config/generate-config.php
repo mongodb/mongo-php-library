d8112c877a08ee88cd38e87e3f4b50b1707840c9
Andreas Braun <andreas.braun@mongodb.com>
2025-04-10 10:17:09 +0200

    Test against stable 2.0 version of the extension

--- .evergreen/config/generate-config.php
+++ .evergreen/config/generate-config.php
@@ -58,9 +58,8 @@ $allFiles[] = generateConfigs('tasks', 'test', 'mongodbVersion', 'csfle.yml', $c
 
 // Test variants
 $allFiles[] = generateConfigs('buildvariants', 'test-variant', 'phpVersion', 'modern-php-full.yml', $supportedPhpVersions);
-// TODO: Re-enable when 2.0.0 is released
-// $allFiles[] = generateConfigs('buildvariants', 'test-variant', 'phpVersion', 'phpc.yml', [$latestPhpVersion]);
-// $allFiles[] = generateConfigs('buildvariants', 'test-variant', 'phpVersion', 'lowest.yml', [$lowestPhpVersion]);
+$allFiles[] = generateConfigs('buildvariants', 'test-variant', 'phpVersion', 'phpc.yml', [$latestPhpVersion]);
+$allFiles[] = generateConfigs('buildvariants', 'test-variant', 'phpVersion', 'lowest.yml', [$lowestPhpVersion]);
 
 echo "Generated config. Use the following list to import files:\n";
 echo implode("\n", array_map('getImportConfig', $allFiles)) . "\n";

3a0779a72050daedfc576c8619a68d50a1619434
Andreas Braun <andreas.braun@mongodb.com>
2026-02-17 16:07:03 +0100

    Refactor withTransaction logic into smaller methods

--- src/Operation/WithTransaction.php
+++ src/Operation/WithTransaction.php
@@ -59,19 +59,10 @@ final class WithTransaction
             try {
                 call_user_func($this->callback, $session);
             } catch (Throwable $e) {
-                if ($session->isInTransaction()) {
-                    $session->abortTransaction();
-                }
+                // If this method returns, this means we're able to retry the entire transaction.
+                $this->checkForRetryableError($session, $e, $startTime);
 
-                if (
-                    $e instanceof RuntimeException &&
-                    $e->hasErrorLabel('TransientTransactionError') &&
-                    ! $this->isTransactionTimeLimitExceeded($startTime)
-                ) {
-                    continue;
-                }
-
-                throw $e;
+                continue;
             }
 
             if (! $session->isInTransaction()) {
@@ -79,36 +70,78 @@ final class WithTransaction
                 return;
             }
 
-            while (true) {
-                try {
-                    $session->commitTransaction();
-                } catch (RuntimeException $e) {
-                    if (
-                        $e->getCode() !== 50 /* MaxTimeMSExpired */ &&
-                        $e->hasErrorLabel('UnknownTransactionCommitResult') &&
-                        ! $this->isTransactionTimeLimitExceeded($startTime)
-                    ) {
-                        // Retry committing the transaction
-                        continue;
-                    }
+            // Commit the transaction and return if it was committed successfully
+            if ($this->commitTransaction($session, $startTime)) {
+                return;
+            }
+        }
+    }
 
-                    if (
-                        $e->hasErrorLabel('TransientTransactionError') &&
-                        ! $this->isTransactionTimeLimitExceeded($startTime)
-                    ) {
-                        // Restart the transaction, invoking the callback again
-                        continue 2;
-                    }
+    /**
+     * Checks if the given exception is an error that allows for retrying the connection
+     *
+     * This method is called when an error happens during the transaction callback. If the error is not retryable, or if
+     * the time limit for retries has been exceeded, it re-throws the caught exception to break out of the transaction
+     * loop. In other cases, it returns without throwing.
+     */
+    private function checkForRetryableError(Session $session, Throwable $e, int $startTime): void
+    {
+        if ($session->isInTransaction()) {
+            $session->abortTransaction();
+        }
 
-                    throw $e;
+        if (
+            $e instanceof RuntimeException &&
+            $e->hasErrorLabel('TransientTransactionError') &&
+            ! $this->isTransactionTimeLimitExceeded($startTime)
+        ) {
+            return;
+        }
+
+        throw $e;
+    }
+
+    /**
+     * Attempts to commit the transaction until it either succeeds, encounters a non-retryable error, or exceeds the
+     * time limit for retries
+     *
+     * This method attempts to commit the transaction, retrying the commit if an unknown commit result was encountered.
+     * If the transaction was committed successfully, it returns true. If a transient transaction error has occurred and
+     * the time limit for retries has not been exceeded, it returns false to indicate that the entire transaction should
+     * be retried. If a none-retryable error is encountered, or if the time limit for retries has been exceeded, it
+     * throws the last exception encountered to break out of the transaction loop.
+     *
+     * @return bool Returns true if the transaction was successfully committed, or false if the transaction should be retried
+     * @throws Throwable if an error occurs while committing the transaction that should not be retried
+     */
+    private function commitTransaction(Session $session, int $startTime): bool
+    {
+        while (true) {
+            try {
+                $session->commitTransaction();
+            } catch (RuntimeException $e) {
+                if (
+                    $e->getCode() !== 50 /* MaxTimeMSExpired */ &&
+                    $e->hasErrorLabel('UnknownTransactionCommitResult') &&
+                    ! $this->isTransactionTimeLimitExceeded($startTime)
+                ) {
+                    // Retry committing the transaction
+                    continue;
                 }
 
-                // Commit was successful
-                break;
+                if (
+                    $e->hasErrorLabel('TransientTransactionError') &&
+                    ! $this->isTransactionTimeLimitExceeded($startTime)
+                ) {
+                    // Restart the transaction, invoking the callback again
+                    return false;
+                }
+
+                throw $e;
             }
 
-            // Transaction was successful
-            break;
+            // Commit was successful, indicate to break out of the transaction loop
+            return true;
         }
     }
 

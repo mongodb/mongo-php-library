9d848436ad5c13532ae607a4de16fe138a379647
Jérôme Tamarelle <jerome@tamarelle.net>
2026-02-26 17:22:48 +0100

    Fix CS

--- generator/src/OperatorTestGenerator.php
+++ generator/src/OperatorTestGenerator.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace MongoDB\CodeGenerator;
 
 use DateTimeImmutable;
+use DateTimeZone;
 use InvalidArgumentException;
 use MongoDB\BSON\Binary;
 use MongoDB\BSON\Decimal128;
@@ -145,7 +146,7 @@ class OperatorTestGenerator extends OperatorGenerator
                 'bson_regex' => new Regex(...(array) $value),
                 'bson_int128' => new Int64($value),
                 'bson_decimal128' => new Decimal128($value),
-                'bson_utcdatetime' => new UTCDateTime(is_numeric($value) ? (int) $value : new DateTimeImmutable($value, new \DateTimeZone('UTC'))),
+                'bson_utcdatetime' => new UTCDateTime(is_numeric($value) ? (int) $value : new DateTimeImmutable($value, new DateTimeZone('UTC'))),
                 'bson_binary' => new Binary(base64_decode($value)),
                 'bson_objectId' => new ObjectId($value),
                 'bson_uuid' => new Binary(hex2bin(str_replace('-', '', $value)), Binary::TYPE_UUID),
